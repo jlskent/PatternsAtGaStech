6a1156ce91068ae278daa8a9eea9f5a100b33297
FashionEnglishName <995862101@qq.com>
2020-02-20 20:34:25 -0600

    add responsive. change to table view

--- js/result.js
+++ js/result.js
@@ -8,24 +8,24 @@ var parent = d3.select("#resultab");
 const p1 = getCreditCardTransactions().then(data =>{
     var result = data.filter(d => {
         return  5>ts(d.timestamp).getHours()>0});
-    var TimeTable = parent.append("div").attr('id', 'TimeTable').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var TimeTable = parent.append("div").attr('id', 'TimeTable').attr('class', 'col-xl-4 col-sm-6 col-12')
                             .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "People go out in the midnight is weird");
     TimeTable.append("h4")
             .text("Unusual activity between 1AM to 4AM");
-    TimeTable.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result)
-    .enter()
-    .append("li")
-    .text((d) => {
-      const fullName = d.FirstName + " " + d.LastName;
-      return fullName + ", " + d.timestamp +", "+ d.location;
-    })
-    .attr("class", "list-group-item")
+    
+    const table = TimeTable.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
 
+    tableHead.append('th').text('Empolyee');
+    tableHead.append('th').text('Timestamp');
+    tableHead.append('th').text('Location');
 
-    //console.log(result)
+    table.append('tbody').selectAll("tr")
+        .data(result)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d.FirstName} ${d.LastName}</td><td>${d.timestamp}</td><td>${d.location}</td>`);
 
 });
 
@@ -49,19 +49,24 @@ const p2 = getCarTrackingData().then(data =>{
         return b.count - a.count;
       });
 
-    var CarTimeTable = parent.append("div").attr('id', 'CarTimeTable').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var CarTimeTable = parent.append("div").attr('id', 'CarTimeTable').attr('class', 'col-xl-4 col-sm-6 col-12')
                                 .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "count gps record showing in the midnight");
     CarTimeTable.append("h4").text("Unusual car activity between 1AM to 4AM");
-    CarTimeTable.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result2)
-    .enter()
-    .append("li")
-    .text((d) => {
-      return "id: "+d.id + "-Count: " + d.count;
-    })
-    .attr("class", "list-group-item")
+    
+
+    const table = CarTimeTable.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
+
+    tableHead.append('th').text('Car Id');
+    tableHead.append('th').text('Activity Count');
+
+
+    table.append('tbody').selectAll("tr")
+        .data(result2)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d.id}</td><td>${d.count}</td>`);
 });
 
 function ts(x){
@@ -76,20 +81,25 @@ const p3 = getCreditCardTransactions().then(data =>{
       });
     var result = data.slice(0, 5)
 
-    var ccHigh = parent.append("div").attr('id', 'ccHigh').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var ccHigh = parent.append("div").attr('id', 'ccHigh').attr('class', 'col-xl-4 col-sm-6 col-12')
                         .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "check large amount of transaction");
     ccHigh.append("h4").text("Highest cc prices");
-    ccHigh.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result)
-    .enter()
-    .append("li")
-    .text((d) => {
-        const fullName = d.FirstName + " " + d.LastName;
-        return fullName + ", " + d.timestamp +", "+ d.location + ", "+d.price;
-    })
-    .attr("class", "list-group-item")
+
+    const table = ccHigh.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
+
+    tableHead.append('th').text('Empolyee Name');
+    tableHead.append('th').text('Timestamp');
+    tableHead.append('th').text('Location');
+    tableHead.append('th').text('Amount');
+
+
+    table.append('tbody').selectAll("tr")
+        .data(result)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d.FirstName} ${d.LastName}</td><td>${d.timestamp}</td><td>${d.location}</td><td>${d.price}</td>`);
 });
 
 const p4 = getLoyaltyCardTransactions().then(data =>{
@@ -98,20 +108,27 @@ const p4 = getLoyaltyCardTransactions().then(data =>{
       });
     var result = data.slice(0, 5)
 
-    var lcHigh = parent.append("div").attr('id', 'lcHigh').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var lcHigh = parent.append("div").attr('id', 'lcHigh').attr('class', 'col-xl-4 col-sm-6 col-12')
                             .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "check large amount of transaction");
     lcHigh.append("h4").text("Highest lc prices");
-    lcHigh.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result)
-    .enter()
-    .append("li")
-    .text((d) => {
-        const fullName = d.FirstName + " " + d.LastName;
-        return fullName + ", " + d.timestamp +", "+ d.location + ", "+d.price;
-    })
-    .attr("class", "list-group-item")
+    
+    
+
+    const table = lcHigh.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
+
+    tableHead.append('th').text('Empolyee Name');
+    tableHead.append('th').text('Date');
+    tableHead.append('th').text('Location');
+    tableHead.append('th').text('Amount');
+
+
+    table.append('tbody').selectAll("tr")
+        .data(result)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d.FirstName} ${d.LastName}</td><td>${d.timestamp}</td><td>${d.location}</td><td>${d.price}</td>`);
 });
 
 
@@ -132,20 +149,24 @@ const p5 = getCreditCardTransactions().then(data =>{
     });
     var result2 = sortable.slice(0, 5)
 
-    var ccLeast = parent.append("div").attr('id', 'ccLeast').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var ccLeast = parent.append("div").attr('id', 'ccLeast').attr('class', 'col-xl-4 col-sm-6 col-12')
                             .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "check small amount of transaction");
     ccLeast.append("h4").text("Least cc records location");
-    ccLeast.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result2)
-    .enter()
-    .append("li")
-    .text((d) => {
-        
-        return d[0] + ", " +d[1] ;
-    })
-    .attr("class", "list-group-item")
+    
+
+    const table = ccLeast.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
+
+    tableHead.append('th').text('Place Name');
+    tableHead.append('th').text('Transaction Count');
+
+
+    table.append('tbody').selectAll("tr")
+        .data(result2)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d[0]}</td><td>${d[1]}</td>`);
 
     //console.log(sortable)
 });
@@ -169,20 +190,23 @@ const p6 = getCreditCardTransactions().then(data =>{
     });
     var result2 = sortable.slice(0, 10)
 
-    var ccMost = parent.append("div").attr('id', 'ccMost').attr('class', 'col-xl-4 col-6 col-sm-12')
+    var ccMost = parent.append("div").attr('id', 'ccMost').attr('class', 'col-xl-4 col-sm-6 col-12')
                         .attr('data-toggle', 'tooltip').attr('data-placement', "top").attr("title", "check where people do most transactions");
     ccMost.append("h4").text("Most cc records location");
-    ccMost.append("ul")
-    .attr("class", "list-group")
-    .selectAll("li")
-    .data(result2)
-    .enter()
-    .append("li")
-    .text((d) => {
-        
-        return d[0] + ", " +d[1] ;
-    })
-    .attr("class", "list-group-item")
+    
+    const table = ccMost.append("table")
+    .attr("class", "table table-hover");
+    const tableHead = table.append('thead').append('tr');
+
+    tableHead.append('th').text('Place Name');
+    tableHead.append('th').text('Transaction Count');
+
+
+    table.append('tbody').selectAll("tr")
+        .data(result2)
+        .enter()
+        .append("tr")
+        .html((d) => `<td>${d[0]}</td><td>${d[1]}</td>`);
 
     //console.log(sortable)
 });
